7c91f420d119bda78443cff0657f7cc90edc01af
Lai Hop Sam <laihopsam02051998@gmail.com>
2021-12-01 13:39:40 +0700

    create CarDetailing

---
+++ src/containers/CarDetailing/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../../components/Banner";
+
+function CarDetailing() {
+  return (
+    <div>
+      <Banner
+        imageBanner={"https://wrapstylevietnam.com/images/media/50.jpg"}
+        titleBanner={"CAR DETAILING"}
+      />
+    </div>
+  );
+}
+export default CarDetailing;
